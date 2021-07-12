f9326c4746ffdd02995360547b080e984a411eb5
Regourd Colin <colin.regourd@inetum.world>
2021-07-12 10:46:38 +0200

    error on attribute created_at

--- src/client.js
+++ src/client.js
@@ -23,7 +23,7 @@ const client = mozaik => {
 					id : x.short_id,
 					author : x.author_name.split('.').map(e=>e[0].toUpperCase()+e.slice(1)).join(' '),
 					msg : x.message,
-					date : x.createdAt
+					date : x.created_at
 				}
 			}))
 			.catch(err => err)
